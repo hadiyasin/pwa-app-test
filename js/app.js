6067224d920180272bb996d3e7bf6c5c6bf6a8c7
hadiyasin <ayasin@sompo-tlv.io>
2023-09-13 16:23:53 +0300

    Basic form setup

--- js/app.js
+++ js/app.js
@@ -2,6 +2,24 @@ console.log('I am running!');
 
 $(document).ready(function(){
     console.log('Document ready!');
+
+    $("#cropYearPicker").datepicker({
+        format: "yyyy",
+        viewMode: "years",
+        startView: "years",
+        minViewMode: "years",
+        maxViewMode: "years",
+        clearBtn: false,
+        autoclose: true,
+        todayHighlight: false,
+        datesDisabled: [],
+        toggleActive: true
+    });
+
+    $("#phoneNumberInput").intlTelInput({
+        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.4.6/js/utils.js"
+      });
+
     $("#sbmt").click(function(){
         console.log('Clicked!');
         let emailSubject = 'Subject';
