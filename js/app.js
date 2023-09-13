82a53f73d22f82a5b9ad51687db5bda01865b492
hadiyasin <ayasin@sompo-tlv.io>
2023-09-13 14:26:15 +0300

    Testing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log('I am running!');
+
+$(document).ready(function(){
+    console.log('Document ready!');
+    $("#sbmt").click(function(){
+        console.log('Clicked!');
+        let emailSubject = 'Subject';
+        let emailBody = 'Body';
+        window.location.href = "mailto:support@example.com?subject=" + emailSubject + "&body=" + emailBody;
+        // $(this).hide();
+    });
+});
